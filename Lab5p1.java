7b5ece876e2e2becf9cef113885fc5f302bc559a
sapavathreethu090 <sapavathreethu09@gmail.com>
2025-10-15 10:32:05 +0530

    Add files via upload

---
+++ Lab5p1.java
@@ -0,0 +1,23 @@
+📓import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println(x/y);
+        }
+        catch(InputMismatchException ie){
+            System.out.println(ie.getClass().getName());
+        }
+        catch(ArithmeticException ie){
+         System.out.println(ie.getClass().getName()+":"+ie.getMessage());
+        }
+    }
+}
